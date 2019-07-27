4af6dc0dd834d024ba56bde4d4e2d4f40b3999fd
zhoodar8 <tokyo-95@mail.ru>
2019-07-27 13:13:07 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        String names[] = new String[3];
+        names[0] = "Ban";
+        names[1] = "Mick";
+        names[2] = "Ford";
+
+
+        for (String name : names) {
+
+
+            switch (name) {
+
+                case "Ban":
+                    System.out.println("Доброе утро!" + name);
+                    break;
+
+                case "Mick":
+                    System.out.println("Добрый день!" + name);
+                    break;
+
+                case "Ford":
+                    System.out.println("Добрый вечер!" + name);
+                    break;
+
+            }
+        }
+    }
+
+}
+
